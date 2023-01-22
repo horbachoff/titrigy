0a394c525fc97e54f4ff02bc61d452c5153dbc62
horbachoff <mr.horbachov@gmail.com>
2023-01-22 17:19:58 -0500

    Forms for Add Employee and Generater Weekly Schedule

--- titrigy/app.py
+++ titrigy/app.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -5,9 +6,29 @@ from wtforms import StringField, SubmitField
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
+employees = ["Yehor", "Eden", "Annalise", "Noel", "Uri", "Pius", "Hugo"]
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+class EmployeeForm(FlaskForm):
+    employee = StringField("Employee Name:")
+    submit = SubmitField("add")
+
+#rendering list of employees only
+# @app.route('/', methods=["GET", "POST"])
+# def index():
+#     if 'employee' in request.form:
+#         employees.append(request.form['employee'])
+#     return render_template("index.html", employees=employees, template_form=EmployeeForm(), weekdays=weekdays)
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if 'employee' in request.form:
+        employees.append(request.form['employee'])
+    schedule = {}
+    random.shuffle(employees)
+    for i, weekday in enumerate(weekdays):
+        schedule[weekday] = employees[i]
+    return render_template("index.html", employees=employees, template_form=EmployeeForm(), weekdays=weekdays, schedule=schedule)
 
 if __name__ == "__main__":
     app.run(debug=True)
